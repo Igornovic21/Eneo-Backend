e4a46b41147c5ed0cbdc7b25982a80fd5ad3aea0
Igornovic21 <kamguefokoigor@gmail.com>
2025-02-07 15:20:40 +0100

    fix: change itinary loading in database

--- script.py
+++ script.py
@@ -214,18 +214,18 @@ def import_odk_xlsx_data():
 BANOC_URL="https://dev.api.banoc.coeurduweb.com/api/geocode/coordinates?latitude={}&longitude={}&level=155"
 
 def add_code_banoc_soa():
-    file_name = 'PROPRIETAIRE NB.geojson'
+    file_name = 'batiments scolaire.geojson'
     # Load GEO JSON data
     with open(file_name, 'r', encoding='utf-8') as file:
         geo_json_data = json.load(file)
 
-    itinaries = geo_json_data['features']
+    features = geo_json_data['features']
 
     new_features = []
-    for element in itinaries:
-        lattitude = element["properties"]["Y"]
-        longitude = element["properties"]["X"]
+    for element in features:
         try:
+            lattitude = element["geometry"]["coordinates"][1]
+            longitude = element["geometry"]["coordinates"][0]
             response = requests.get(BANOC_URL.format(lattitude, longitude))
             if response.status_code == 200:
                 data = response.json()
@@ -235,6 +235,7 @@ def add_code_banoc_soa():
                 element["properties"]["BANOC_CODE"] = "Unknown"
             new_features.append(element)
         except Exception as e:
+            element["properties"]["BANOC_CODE"] = "Invalid"
             print(e)
 
     with open('NEW_{}'.format(file_name), 'w') as file:
@@ -242,8 +243,5 @@ def add_code_banoc_soa():
         json.dump(geo_json_data, file, separators=(",", ":"), indent=None)
 
 if __name__ == "__main__":
-    text = "Démantelé"
-    fixed_text = text.encode('latin1').decode('utf-8')
-    print(fixed_text)
-    # add_code_banoc_soa()
+    add_code_banoc_soa()
     # pass
